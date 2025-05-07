d4457f9a3775e7f9a600a85e9c8c636aec8dc21a
marcosfreitas221 <marcosfreitas7542@gmail.com>
2025-05-07 19:11:40 -0300

    Introduction to Webpack

---
+++ JavaScript/Webpack/public/assets/js/bundle.js
@@ -0,0 +1,14 @@
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log("Olá mundo");
+function soma(x, y) {
+  var sum = 0;
+  sum = x + y;
+  return console.log(sum);
+}
+soma(10, 20);
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
---
+++ JavaScript/Webpack/src/index.js
@@ -0,0 +1,9 @@
+console.log("Olá mundo")
+
+function soma(x, y) {
+    let sum = 0;
+    sum = x + y
+    return console.log(sum)
+}
+
+soma(10, 20)
