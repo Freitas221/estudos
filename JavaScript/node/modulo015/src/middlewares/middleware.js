7e4e2a227c47a22ff699f34fd65c66efad0d9c37
marcosfreitas221 <marcosfreitas7542@gmail.com>
2025-11-06 19:35:44 -0300

    add modulo015

---
+++ JavaScript/node/modulo015/src/middlewares/middleware.js
@@ -0,0 +1,12 @@
+exports.middleware = (req, res, next) => {
+    if(req.body.cliente) {
+        console.log()
+        console.log(`Objeto postado ${req.body.cliente}`)
+    }
+    next()
+}
+
+exports.middlewareGlobal = (req, res, next) => {
+    console.log(req.session.usuario)
+    next()
+}
